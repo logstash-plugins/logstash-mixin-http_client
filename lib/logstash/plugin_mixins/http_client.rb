181fa0424d9e0adaad505642ad4986b751b62723
Andrew Cholakian <andrew@andrewvc.com>
2015-07-09 11:46:20 -0500

    Keepalive should be a bool parameter

--- lib/logstash/plugin_mixins/http_client.rb
+++ lib/logstash/plugin_mixins/http_client.rb
@@ -35,7 +35,7 @@ module LogStash::PluginMixins::HttpClient
 
     # Specify this if you want to keep the TCP connection open between requests
     # using HTTP Keep-Alive. Defaults to 0 (off)
-    config :keepalive, :validate => :number, :default => 0
+    config :keepalive, :validate => :boolean, :default => true
 
     # How many times should the client retry a failing URL? Default is 3
     config :automatic_retries, :validate => :number, :default => 3
