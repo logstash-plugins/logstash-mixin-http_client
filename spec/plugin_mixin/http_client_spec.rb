f7f12a738b8bcb21244e677c8d5a72be13122737
Karol Bucek <kares@users.noreply.github.com>
2022-01-25 13:12:30 +0100

    Update spec/plugin_mixin/http_client_spec.rb
    Co-authored-by: João Duarte <jsvd@users.noreply.github.com>

--- spec/plugin_mixin/http_client_spec.rb
+++ spec/plugin_mixin/http_client_spec.rb
@@ -194,7 +194,7 @@ describe LogStash::PluginMixins::HttpClient do
     context "default" do
       let(:conf) { basic_config }
 
-      it "sets manticore verify" do
+      it "sets manticore verify to :strict" do
         expect( Dummy.new(conf).client_config[:ssl] ).to include :verify => :strict
       end
     end
